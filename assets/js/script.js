e1002184c9f8a8139ab7fb943194918c31979324
SRINATH2654 <163601691+SRINATH2654@users.noreply.github.com>
2025-09-05 17:35:39 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-// script.js content
+// Load navbar and footer dynamically
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("partials/navbar.html")
+        .then(res => res.text())
+        .then(data => document.getElementById("navbar").innerHTML = data);
+
+    fetch("partials/footer.html")
+        .then(res => res.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+});
